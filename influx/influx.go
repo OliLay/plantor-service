9560752b4095f0c2ab984f8849d6f4da48e7b272
Oliver Layer <olilay@live.de>
2020-11-01 12:07:54 +0100

    removed influx setup via plantor.go

--- influx/influx.go
+++ influx/influx.go
@@ -6,7 +6,6 @@ import (
 	influx "github.com/influxdata/influxdb-client-go/v2"
 	influxapi "github.com/influxdata/influxdb-client-go/v2/api"
 	influxwrite "github.com/influxdata/influxdb-client-go/v2/api/write"
-	influxdomain "github.com/influxdata/influxdb-client-go/v2/domain"
 	"log"
 	"strings"
 	"time"
@@ -17,23 +16,6 @@ const OrganizationName = "plantor_org"
 
 func Connect(host string, port int64) influx.Client {
 	return influx.NewClient(fmt.Sprintf("http://%s:%d", host, port), "")
-}
-
-func Setup(client influx.Client) {
-	// TODO: fix
-	//orgApi := client.OrganizationsAPI()
-	//var organization *influxdomain.Organization = nil
-	//var exists bool
-	//if organization, exists = organizationExists(orgApi); !exists {
-	//	log.Printf("Organization %s doesn't exist, creating it!", OrganizationName)
-	//	organization = createOrganization(orgApi)
-	//}
-
-	//bucketsApi := client.BucketsAPI()
-	//if !bucketExists(bucketsApi) {
-	//	log.Printf("Bucket %s doesn't exist, creating it!", BucketName)
-	//	createBucket(bucketsApi, organization)
-	//}
 }
 
 func GetWriteApi(client influx.Client) influxapi.WriteAPIBlocking {
@@ -68,29 +50,4 @@ func PersistMeasurement(influxWriteApi influxapi.WriteAPIBlocking, point *influx
 
 func stringifyKey(key string) string {
 	return strings.Replace(key, "/", "-", -1)
-}
-
-func organizationExists(influxOrgApi influxapi.OrganizationsAPI) (*influxdomain.Organization, bool) {
-	organization, err := influxOrgApi.FindOrganizationByName(context.Background(), OrganizationName)
-	return organization, err == nil
-}
-
-func bucketExists(influxBucketApi influxapi.BucketsAPI) bool {
-	_, err := influxBucketApi.FindBucketByName(context.Background(), BucketName)
-	return err == nil
-}
-
-func createOrganization(influxOrgApi influxapi.OrganizationsAPI) *influxdomain.Organization {
-	organization, err := influxOrgApi.CreateOrganizationWithName(context.Background(), OrganizationName)
-	if err != nil {
-		log.Fatalf("Could not create organization '%s' on InfluxDb: %s", OrganizationName, err)
-	}
-	return organization
-}
-
-func createBucket(influxBucketApi influxapi.BucketsAPI, organization *influxdomain.Organization) {
-	_, err := influxBucketApi.CreateBucketWithName(context.Background(), organization, BucketName)
-	if err != nil {
-		log.Fatalf("Could not create bucket '%s' on InfluxDb: %s", BucketName, err)
-	}
 }
